4beab2e6722458ca731df2f56737d1653ffa20e4
VeeShostak <VeeShostak@users.noreply.github.com>
2017-12-05 01:00:05 -0500

    Build optimal model using Backward Elimination

--- 2-Regression/2-Multiple-Linear-Regression/multiple-linear-regression.py
+++ 2-Regression/2-Multiple-Linear-Regression/multiple-linear-regression.py
@@ -56,3 +56,65 @@ regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
+
+# =================================
+
+# =============================================================================
+# Backward Elimination: (remove indep vars that are above sig level)
+# choose significance level (SL = 0.005)
+# push all independent vars into model (All In) (fit full model with all possible predictors)
+# Consider Predictor with highest P-value, if p > SL got to step 4, else go to finish
+# remove the var with highest P value (remove the predictor)
+# rebuild. fit model without the variable (then go to step 3)
+
+# p-value is a probability that we did not compute our coefficient correctly (rough explanation)
+# (the lower the p value of indep var the more sig your indep var will be with dependent var.)
+# =============================================================================
+
+
+# Build the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+
+# curr library thinks our eq is y = b1x1 + b2x2 + b3x3 + b4D1 + b5(1 - D1
+# append x0b0     (we'll add a columns of 1's, x0 = 1)
+# x = add matrix of features X to 1 column of 50 ones
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1) # axis = 1. columns
+
+# ============================
+# START Backward Elimination
+# ============================
+
+# optimal matrix of features. (predictors where p < SL)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]] # push all independent vars into model (All In)
+
+# push all independent vars into model (All In) (fit full model with all possible predictors)
+# ordinary least squares regressor
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+# Consider Predictor with highest P-value, if p > SL got to step 4, else go to finish
+regressor_OLS.summary() # display each ind var p valu (and other infp)
+
+
+#x2 has highest (95%) p value
+# so remove x2
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+
+# ... complete Backward Elimination
+regressor_OLS.summary()
+X_opt = X[:, [0, 3, 4, 5]] # removed x1, 94%
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+
+regressor_OLS.summary()
+X_opt = X[:, [0, 3, 5]] # removed x2, 60% NOTE: use R sqaured and adjusted R squared to know if to keep marketing spen
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+
+regressor_OLS.summary()
+X_opt = X[:, [0, 3]] # removed x4, 60%
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary() # R&D Spent
+
+
+
+# ============================
+# END Backward Elimination
+# ============================
